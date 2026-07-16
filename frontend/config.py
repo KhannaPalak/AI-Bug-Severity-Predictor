36969759a6095673b7805355528e0c3528f6eb46
Ahana274-Codes <aroraahana160306@gmail.com>
2026-07-16 13:28:02 +0530

    Added health endpoint

--- frontend/config.py
+++ frontend/config.py
@@ -1,11 +1,2 @@
 # Change your API target URL inside your Streamlit code to this:
-import os
-
-# 1. First, check if Render has set an environment variable (like PORT or RENDER)
-# 2. If it's running on Render, use the cloud backend URL
-# 3. If it's running on your computer, default to localhost (127.0.0.1)
-
-if os.environ.get("RENDER") or os.environ.get("PORT"):
-    API_URL = "https://bug-backend-service.onrender.com"
-else:
-    API_URL = "http://127.0.0.1:8000"
+API_URL = "https://bug-backend-service.onrender.com/predict"
